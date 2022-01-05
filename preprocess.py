b18befb43d2d79ca4b9747fcad3571e2416d4173
ZeZeZe99 <rhuang@oberlin.edu>
2022-01-05 00:40:48 -0500

    preprocess images

--- preprocess.py
+++ preprocess.py
@@ -1,61 +1,63 @@
+"""
+This program is used to preprocess images to 256*256 jpg format.
+"""
+
 import os
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
-from PIL import Image
 
 import torch
 import torch.nn as nn
 import torchvision.transforms as T
 from torchvision.io import read_image, write_jpeg
-from torchvision.utils import save_image
-from torch.utils.data import Dataset, DataLoader
 
 
-plt.rcParams["savefig.bbox"] = 'tight'
-torch.manual_seed(1)
+# display images
+# plt.rcParams["savefig.bbox"] = 'tight'
+# torch.manual_seed(1)
+#
+#
+# def show(imgs):
+#     fix, axs = plt.subplots(ncols=len(imgs), squeeze=False)
+#     for i, img in enumerate(imgs):
+#         img = T.ToPILImage()(img.to('cpu'))
+#         axs[0, i].imshow(np.asarray(img))
+#         axs[0, i].set(xticklabels=[], yticklabels=[], xticks=[], yticks=[])
+#     plt.show()
 
 
-def show(imgs):
-    fix, axs = plt.subplots(ncols=len(imgs), squeeze=False)
-    for i, img in enumerate(imgs):
-        img = T.ToPILImage()(img.to('cpu'))
-        axs[0, i].imshow(np.asarray(img))
-        axs[0, i].set(xticklabels=[], yticklabels=[], xticks=[], yticks=[])
-    plt.show()
-
-
+# transformation: first resize the smaller dimension to 256, then crop the center 256*256 square
 transforms = nn.Sequential(
     T.Resize(256),
     T.CenterCrop(256)
 )
 
 if __name__ == '__main__':
+
+    # get input and output directory from keyboard input
+    input_path = sys.argv[1]
+    output_path = sys.argv[2]
     # get filenames
-    path = 'train_2/'
-    img_names = os.listdir(path)
+    img_names = os.listdir(input_path)
+
+    count = 0
     for i in range(len(img_names)):
         filename = img_names[i]
         try:
-            img = read_image(path + filename)
+            img = read_image(input_path + '/' + filename)
+            # perform resizing and cropping
             t_img = transforms(img)
+            # save the output jpeg file to a different directory with the same name
+            write_jpeg(t_img, output_path + '/' + filename)
 
-            t_img = T.ToPILImage()(t_img)
-
-            if t_img.mode != 'RGB' and t_img.mode != 'L':
-                t_img = t_img.convert("RGB")
-            t_img.save('processed/' + filename)
-        except:
+            # another way to save the jpeg file
+            # t_img = T.ToPILImage()(t_img)
+            # if t_img.mode != 'RGB' and t_img.mode != 'L':
+            #     t_img = t_img.convert("RGB")
+            # t_img.save('processed/' + filename)
+        except Exception as e:
             print(filename)
-
-
-
-
-    # img = read_image(path + '2.jpg')
-    # show([img1,t_img1, t_img2, t_img3])
-    #
-    #
-    # # write_jpeg(t_img1, 'processed/' + filename)
-    # save_image(t_img1, 'proccessed/' + filename)
-    # # torch.save(t_img1, 'proccessed/'+filename)
-    # # Image.fromarray(t_img1.numpy()).save('a.jpg')
-    # img2 = read_image('proccessed/'+filename)
+            print(e)
+            count += 1
+    print('Failure: %d' % count)
